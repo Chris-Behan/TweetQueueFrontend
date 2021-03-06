f7a29eea2084152bcbca71793c1bf731de9ddb54
Chris Behan <behan@hey.com>
2021-03-06 18:16:41 +0000

    utilize http only cookies for credentials.

--- login.js
+++ login.js
@@ -1,9 +1,12 @@
 async function login() {
   try {
+    // Query login endpoint to get twitter login link
     const response = await fetch('https://TweetQueueBackend.behan.repl.co/login', {
       method: 'GET',
     })
     const twitterLoginLink = await response.text()
+    // Open twitter login link
+    // Login link will redirect to home page on successful login
     window.location.replace(twitterLoginLink);
   } catch (err) {
     console.log('error ocurred!')
--- queue.js
+++ queue.js
@@ -1,28 +1,79 @@
 const urlParams = new URLSearchParams(window.location.search);
+// Extract oauth credentials from url parameters if redirected from twitter login
 const oauthToken = urlParams.get('oauth_token');
 const oauthVerifier = urlParams.get('oauth_verifier');
-
-async function getUser() {
-  try {
-    data = { oauth_verifier: oauthVerifier,
-            oauth_token: oauthToken }
-    const response = await fetch('https://TweetQueueBackend.behan.repl.co/getUser', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(data)
-      
-    })
-    const user = await response.json();
+const test1 = urlParams.get('nonexists')
+if (oauthToken && oauthVerifier) {
+    const user = getUserInfoandSetCredentials(oauthToken, oauthVerifier);
     console.log(user);
-  } catch (err) {
-    console.log('error ocurred!')
-    console.log(err)
-  }
+} else {
+    // Load user info attempt to load user info from local storage
+    localStorage.getItem('user')
 }
-getUser();
 
-function test(){
-  alert('test!')
+/**
+ * Query for user info and credentials.
+ * user info will be returned in body of request,
+ * credentials will be set as an http-only cookie.
+ */
+async function getUserInfoandSetCredentials(oauthToken, oauthVerifier) {
+    try {
+        data = {
+            oauth_verifier: oauthVerifier,
+            oauth_token: oauthToken
+        }
+        const response = await fetch('https://TweetQueueBackend.behan.repl.co/getUser', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data),
+            credentials: 'include'
+        })
+        const user = await response.json();
+        console.log(`User info retrieved: ${user}`)
+        return user
+    } catch (e) {
+        console.log(`Error sending POST to /getUser: ${e}`)
+    }
+}
+
+async function test() {
+    try {
+        data = { 'text': 'hello world' }
+        const response = await fetch('https://TweetQueueBackend.behan.repl.co/tweet', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data),
+            credentials: 'same-origin'
+
+        })
+        const user = await response.text();
+        console.log(user);
+    } catch (err) {
+        console.log('error ocurred!')
+        console.log(err)
+    }
+}
+
+async function addTweet() {
+    try {
+        data = { 'text': 'hello world' }
+        const response = await fetch('https://TweetQueueBackend.behan.repl.co/tweets', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data),
+            credentials: 'include'
+
+        })
+        const user = await response.text();
+        console.log(user);
+    } catch (err) {
+        console.log('error ocurred!')
+        console.log(err)
+    }
 }
